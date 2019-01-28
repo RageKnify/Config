2b071b03c3923a5bec9400b5ac20f0e08be5327d
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2019-01-28 13:33:36 +0000

    Changes .zsh_colors to .colors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-source /home/jp/.zsh_colors
+source /home/jp/.colors
 polybar --config=/home/jp/.config/polybar/config -r barBottom &
 
 echo "Bars launched..."

924a5efe003ff695ebfa625e0438ab5f5ca1e6a9
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2019-06-03 21:43:33 +0100

    Make polybar/launch.sh POSIX compliant

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,10 +4,10 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$(id -ru)" -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-source /home/jp/.colors
+. /home/jp/.colors
 polybar --config=/home/jp/.config/polybar/config -r barBottom &
 
 echo "Bars launched..."

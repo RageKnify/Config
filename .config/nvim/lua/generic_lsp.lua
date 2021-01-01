481cd944e0e190942d1672b82dc97476a51c3ac1
RageKnify <RageKnify@gmail.com>
2021-01-01 02:01:32 +0000

    nvim: add bind and CursorHold autocommand for lsp hover

--- .config/nvim/lua/generic_lsp.lua
+++ .config/nvim/lua/generic_lsp.lua
@@ -1,4 +1,6 @@
 return function(client)
 	-- [[ other on_attach code ]]
+	vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
+	vim.cmd [[autocmd CursorHold <buffer> :lua vim.lsp.buf.hover()]]
 	require 'illuminate'.on_attach(client)
 end

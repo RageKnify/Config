931ab75bd6fc0f384cc595000e3d4bff5f525d53
RageKnify <RageKnify@gmail.com>
2021-02-09 18:12:51 +0000

    nvim: Add illuminate.next_reference binding

--- .config/nvim/lua/generic_lsp.lua
+++ .config/nvim/lua/generic_lsp.lua
@@ -2,5 +2,9 @@ return function(client)
 	-- [[ other on_attach code ]]
 	vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
 	vim.cmd [[autocmd CursorHold <buffer> :lua vim.lsp.buf.hover()]]
+
+	-- illuminate stuff
+	vim.api.nvim_buf_set_keymap(0, 'n', '<leader>gn', '<cmd>lua require"illuminate".next_reference{}<cr>', {noremap = true})
+	vim.api.nvim_buf_set_keymap(0, 'n', '<leader>gp', '<cmd>lua require"illuminate".next_reference{reverse=true}<cr>', {noremap = true})
 	require 'illuminate'.on_attach(client)
 end

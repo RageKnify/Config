92eb840e4ee6cde8837aac02238e104161f6f9e2
RageKnify <RageKnify@gmail.com>
2021-05-12 19:49:54 +0100

    scripts: relaunch polybar after updating configs

--- scripts/update_configs.sh
+++ scripts/update_configs.sh
@@ -4,3 +4,4 @@ cat /home/jp/.config/polybar/base /home/jp/.config/polybar/$(hostname) > /home/j
 cat /home/jp/.config/sxhkd/base /home/jp/.config/sxhkd/$(hostname) > /home/jp/.config/sxhkd/sxhkdrc
 cat /home/jp/.config/i3/colors /home/jp/.config/i3/base /home/jp/.config/i3/$(hostname) > /home/jp/.config/i3/config
 i3-msg reload
+/home/jp/.config/polybar/launch.sh

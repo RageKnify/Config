8835ef57f88c8fd30f05c20c8a0feb6936445791
RageKnify <RageKnify@gmail.com>
2022-02-25 18:39:23 +0000

    nix: join split user configs

--- nix/hosts/lazarus/configuration.nix
+++ nix/hosts/lazarus/configuration.nix
@@ -10,10 +10,6 @@
   networking.hostName = "lazarus";
   networking.domain = "jplborges.pt";
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2sdJFvvnEIYztPcznXvKpY4vOWedZ1qzDaAgRxrczS jp@war"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9ckbhT0em/dL75+RV+sdqwbprRC9Ff/MoqqpBgbUSh jp@pestilence"
-  ];
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
@@ -24,11 +20,21 @@
   nix.trustedUsers = [ "root" "@wheel" ];
   users = {
     mutableUsers = true;
-    users.jp = {
-      isNormalUser = true;
-      createHome = true;
-      shell = pkgs.fish;
-      extraGroups = [ "wheel" ];
+    users = {
+      root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2sdJFvvnEIYztPcznXvKpY4vOWedZ1qzDaAgRxrczS jp@war"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9ckbhT0em/dL75+RV+sdqwbprRC9Ff/MoqqpBgbUSh jp@pestilence"
+      ];
+      jp = {
+        isNormalUser = true;
+        createHome = true;
+        shell = pkgs.fish;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2sdJFvvnEIYztPcznXvKpY4vOWedZ1qzDaAgRxrczS jp@war"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9ckbhT0em/dL75+RV+sdqwbprRC9Ff/MoqqpBgbUSh jp@pestilence"
+        ];
+      };
     };
   };
 }

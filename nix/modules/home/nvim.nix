4281f26dc988b8deb99d21af8047fd4caa4e3638
RageKnify <RageKnify@gmail.com>
2022-02-28 15:36:43 +0000

    nix: nvim: fix EDITOR and vimdiff

--- nix/modules/home/nvim.nix
+++ nix/modules/home/nvim.nix
@@ -8,9 +8,10 @@
 { pkgs, ... }:
 {
   programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
   programs.neovim.viAlias = true;
   programs.neovim.vimAlias = true;
+  programs.neovim.vimdiffAlias = true;
+  home.sessionVariables.EDITOR = "nvim";
 
   programs.neovim.configure = {
     customRC = ''

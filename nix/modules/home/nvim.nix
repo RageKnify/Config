885b8ef5aacb7ce8d2292cb9e9d3b774e6c5bda7
RageKnify <RageKnify@gmail.com>
2022-02-28 15:17:21 +0000

    nix: use unstable neovim instead of latest

--- nix/modules/home/nvim.nix
+++ nix/modules/home/nvim.nix
@@ -8,7 +8,7 @@
 { pkgs, ... }:
 {
   programs.neovim.enable = true;
-  # programs.neovim.defaultEditor = true;
+  programs.neovim.defaultEditor = true;
   programs.neovim.viAlias = true;
   programs.neovim.vimAlias = true;
 
--- nix/overlays/overrides.nix
+++ nix/overlays/overrides.nix
@@ -7,5 +7,5 @@
 
 self: super: {
   discord = super.latest.discord;
-  neovim = super.latest.neovim;
+  neovim = super.unstable.neovim;
 }

2b7debb2d31248da39eac65cd6f54b8e9ca2c749
João Rocha <78209305+Calhau18@users.noreply.github.com>
2022-03-04 17:36:29 +0000

    scripts: use $HOME in update_configs.sh (#1)

--- scripts/update_configs.sh
+++ scripts/update_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cat /home/jp/.config/polybar/base /home/jp/.config/polybar/$(hostname) > /home/jp/.config/polybar/config
-cat /home/jp/.config/sxhkd/base /home/jp/.config/sxhkd/$(hostname) > /home/jp/.config/sxhkd/sxhkdrc
-cat /home/jp/.config/i3/colors /home/jp/.config/i3/base /home/jp/.config/i3/$(hostname) > /home/jp/.config/i3/config
+cat $HOME/.config/polybar/base $HOME/.config/polybar/$(hostname) > $HOME/.config/polybar/config
+cat $HOME/.config/sxhkd/base $HOME/.config/sxhkd/$(hostname) > $HOME/.config/sxhkd/sxhkdrc
+cat $HOME/.config/i3/colors $HOME/.config/i3/base $HOME/.config/i3/$(hostname) > $HOME/.config/i3/config
 i3-msg reload
-/home/jp/.config/polybar/launch.sh
+$HOME/.config/polybar/launch.sh

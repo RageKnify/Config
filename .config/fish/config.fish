51ddd5e7520006b63a3126ee416939f438eb633f
RageKnify <RageKnify@gmail.com>
2022-03-16 17:46:47 +0000

    fish: opam stuf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ env _ZO_ECHO=1 zoxide init fish | source
 # Ctrl+R history-widget
 # Alt+D cd-widget
 fzf_key_bindings
+
+# opam configuration
+source /home/jp/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true

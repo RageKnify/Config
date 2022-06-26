ef446e93ba28a0a8b8aa38b038626f377a3c6d23
RageKnify <RageKnify@gmail.com>
2022-06-26 16:58:40 +0100

    nix: add podman to lazarus

--- nix/hosts/lazarus/system.nix
+++ nix/hosts/lazarus/system.nix
@@ -6,15 +6,12 @@
 # System configuration.
 
 { pkgs, lib, sshKeys, ... }: {
-  imports = [
-  ];
 
   boot.cleanTmpDir = true;
   networking.domain = "jplborges.pt";
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
-    # permitRootLogin = "no";
     authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
     kbdInteractiveAuthentication = false;
   };
@@ -22,11 +19,12 @@
   users = {
     mutableUsers = true;
     users = {
-      root.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2sdJFvvnEIYztPcznXvKpY4vOWedZ1qzDaAgRxrczS jp@war"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9ckbhT0em/dL75+RV+sdqwbprRC9Ff/MoqqpBgbUSh jp@pestilence"
-      ];
       jp.openssh.authorizedKeys.keys = sshKeys;
     };
+  };
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
   };
 }

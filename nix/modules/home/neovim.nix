ec25a204b6f7dc2ac0e73ce939e0830e29e3579d
RageKnify <RageKnify@gmail.com>
2022-07-27 18:30:34 +0100

    nix: nvim: cleanup lua configs

--- nix/modules/home/neovim.nix
+++ nix/modules/home/neovim.nix
@@ -129,52 +129,51 @@ in
           nvim-web-devicons
           {
             plugin = lualine-nvim;
+            type = "lua";
             config = ''
-            set laststatus=2
-            set showtabline=2
-            set noshowmode
-            lua << EOF
-            require('lualine').setup {
-                    options = {
-                            theme = 'auto',
-                            section_separators = {left='', right=''},
-                            component_separators = {left='', right=''},
-                            icons_enabled = true
-                    },
-                    sections = {
-                            lualine_b = { 'diff' },
-                            lualine_c = {
-                                    {'diagnostics', {
-                                            sources = {nvim_diagnostic},
-                                            symbols = {error = ':', warn =':', info = ':', hint = ':'}}},
-                                    {'filename', file_status = true, path = 1}
-                            },
-                            lualine_x = { 'encoding', {'filetype', colored = false} },
-                    },
-                    inactive_sections = {
-                            lualine_c = {
-                                    {'filename', file_status = true, path = 1}
-                            },
-                            lualine_x = { 'encoding', {'filetype', colored = false} },
-                    },
-                    tabline = {
-                            lualine_a = { 'hostname' },
-                            lualine_b = { 'branch' },
-                            lualine_z = { {'tabs', tabs_color = { inactive = "TermCursor", active = "ColorColumn" } } }
-                    },
-                    extensions = { fzf, fugitive },
-            }
-            if _G.Tabline_timer == nil then
-              _G.Tabline_timer = vim.loop.new_timer()
-            else
-              _G.Tabline_timer:stop()
-            end
-            _G.Tabline_timer:start(0,             -- never timeout
-                                   100,          -- repeat every 1000 ms
-                                   vim.schedule_wrap(function() -- updater function
-                                                        vim.api.nvim_command('redrawtabline')
-                                                     end))
-            EOF
+vim.o.laststatus=2
+vim.o.showtabline=2
+vim.o.showmode=false
+require'lualine'.setup {
+        options = {
+                theme = 'auto',
+                section_separators = {left='', right=''},
+                component_separators = {left='', right=''},
+                icons_enabled = true
+        },
+        sections = {
+                lualine_b = { 'diff' },
+                lualine_c = {
+                        {'diagnostics', {
+                                sources = {nvim_diagnostic},
+                                symbols = {error = ':', warn =':', info = ':', hint = ':'}}},
+                        {'filename', file_status = true, path = 1}
+                },
+                lualine_x = { 'encoding', {'filetype', colored = false} },
+        },
+        inactive_sections = {
+                lualine_c = {
+                        {'filename', file_status = true, path = 1}
+                },
+                lualine_x = { 'encoding', {'filetype', colored = false} },
+        },
+        tabline = {
+                lualine_a = { 'hostname' },
+                lualine_b = { 'branch' },
+                lualine_z = { {'tabs', tabs_color = { inactive = "TermCursor", active = "ColorColumn" } } }
+        },
+        extensions = { fzf, fugitive },
+}
+if _G.Tabline_timer == nil then
+  _G.Tabline_timer = vim.loop.new_timer()
+else
+  _G.Tabline_timer:stop()
+end
+_G.Tabline_timer:start(0,             -- never timeout
+                       100,          -- repeat every 1000 ms
+                       vim.schedule_wrap(function() -- updater function
+                                            vim.api.nvim_command('redrawtabline')
+                                         end))
             '';
           }
 
@@ -201,64 +200,63 @@ in
           luasnip
           {
             plugin = nvim-cmp;
+            type = "lua";
             config = ''
-lua <<EOF
-  -- Setup nvim-cmp.
-  local cmp = require'cmp'
+-- Setup nvim-cmp.
+local cmp = require'cmp'
 
-  cmp.setup({
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
-    }, {
-      { name = 'buffer' },
-    })
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
   })
+})
 
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      -- { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = 'buffer' },
-    })
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    -- { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
   })
+})
 
-  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  })
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
   })
-EOF
+})
             '';
           }
           cmp_luasnip
@@ -277,30 +275,29 @@ EOF
               tree-sitter-rust
               tree-sitter-vim
             ]));
+            type = "lua";
             config = ''
-            lua << EOF
-            -- enable highlighting
-            require'nvim-treesitter.configs'.setup { highlight = { enable = true } }
+-- enable highlighting
+require'nvim-treesitter.configs'.setup { highlight = { enable = true } }
 
-            local function define_fdm()
-              if (require "nvim-treesitter.parsers".has_parser()) then
-                -- with treesitter parser
-                vim.wo.foldexpr="nvim_treesitter#foldexpr()"
-                vim.wo.foldmethod="expr"
-              else
-                -- without treesitter parser
-                vim.wo.foldmethod="syntax"
-              end
-            end
-            vim.api.nvim_create_autocmd({ "FileType" }, { callback = define_fdm })
-            EOF
+local function define_fdm()
+  if (require "nvim-treesitter.parsers".has_parser()) then
+    -- with treesitter parser
+    vim.wo.foldexpr="nvim_treesitter#foldexpr()"
+    vim.wo.foldmethod="expr"
+  else
+    -- without treesitter parser
+    vim.wo.foldmethod="syntax"
+  end
+end
+vim.api.nvim_create_autocmd({ "FileType" }, { callback = define_fdm })
             '';
           }
 
           {
             plugin = nvim-lspconfig;
+            type = "lua";
             config = ''
-lua << EOF
 local lsp = require'lspconfig'
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -317,7 +314,6 @@ lsp.rust_analyzer.setup{
 	capabilities = capabilities,
 	on_attach = require'generic_lsp'
 }
-EOF
             '';
           }
           lsp_extensions-nvim
@@ -352,7 +348,6 @@ EOF
             " colorscheme settings
             set background=light
             colorscheme base16-solarized-light
-            lua require'nvim-treesitter.configs'.setup { highlight = { enable = true } }
             '';
           }
 
@@ -365,6 +360,7 @@ EOF
 
           {
             plugin = nvim-osc52;
+            type = "lua";
             config = ''
 local function copy(lines, _)
   require('osc52').copy(table.concat(lines, '\n'))
@@ -384,7 +380,6 @@ vim.g.clipboard = {
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('x', '<leader>y', '"+y')
             '';
-            type = "lua";
           }
         ];
     };

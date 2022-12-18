ea44c84a01296c546fc66c18c6eefd8735d32d98
RageKnify <RageKnify@gmail.com>
2022-12-18 13:55:21 +0000

    nix: neovim: switch module that gives rust hints

--- nix/modules/home/neovim.nix
+++ nix/modules/home/neovim.nix
@@ -215,7 +215,7 @@ lsp.texlab.setup{
 }
       '';
     }
-    lsp_extensions-nvim
+    rust-tools-nvim
 
     {
       plugin = presence-nvim;
@@ -429,23 +429,16 @@ in
 setlocal omnifunc=v:lua.vim.lsp.omnifunc
 
 lua << EOF
-local inlay_hints = require('lsp_extensions').inlay_hints
+local rt = require("rust-tools")
 
--- Global function, so you can just call it on the lua side
-ShowHintsLine = function()
-  inlay_hints {
-    only_current_line = true
-  }
-end
-
-ShowHintsFile = function()
-  inlay_hints()
-end
-EOF
-" not working for some reason
-" autocmd CursorHold,CursorHoldI *.rs :lua ShowHintsLine()
-
-nnoremap <silent> <leader>h <cmd>lua ShowHintsFile()<CR>
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+    end,
+  },
+})
     '';
     home.file."${config.xdg.configHome}/nvim/lua/generic_lsp.lua".text = ''
 return function(client)

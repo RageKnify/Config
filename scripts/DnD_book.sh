b94d67e6d1d8aa058e91af19d92d24ccfcc1ac93
RageKnify <RageKnify@gmail.com>
2023-01-01 20:29:00 +0000

    scripts: remove DnD_book.sh
    Has been ported to the nix config

--- scripts/DnD_book.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /home/jp/Documents/DnD/Books/
-book=$(ls -1 *.pdf | dmenu -i -l 10 -p "Which book to open?" )
-echo -n $book
-if [ -n "$book" ] && [ -f "$book" ] ; then
-	zathura $book;
-fi

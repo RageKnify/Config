1c3b6d7b2f11505fe0a79cc97d7f86a91e5bb864
RageKnify <RageKnify@gmail.com>
2023-02-19 16:15:53 +0000

    nix: nvim: remove cmp from cmdline
    It made neovim's default suggestions stop working

--- nix/modules/home/neovim.nix
+++ nix/modules/home/neovim.nix
@@ -321,15 +321,6 @@ cmp.setup.cmdline('/', {
     { name = 'buffer' },
   }
 })
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' },
-    { name = 'cmdline' },
-  })
-})
       '';
     }
     cmp_luasnip

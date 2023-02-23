6529b38f450204a7875abf643f303aadaa6c4513
RageKnify <RageKnify@gmail.com>
2023-02-23 14:08:13 +0000

    remove old script for modular configs

--- update_configs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cat /home/jp/.config/polybar/base /home/jp/.config/polybar/$(hostname) > /home/jp/.config/polybar/config
-cat /home/jp/.config/sxhkd/base /home/jp/.config/sxhkd/$(hostname) > /home/jp/.config/sxhkd/sxhkdrc
-cat /home/jp/.config/i3/colors /home/jp/.config/i3/base /home/jp/.config/i3/$(hostname) > /home/jp/.config/i3/config
-i3-msg reload

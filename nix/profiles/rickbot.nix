216f33c60a10674f241921af7a5015bd8798c7e1
RageKnify <RageKnify@gmail.com>
2023-07-03 17:14:16 +0100

    nix: fmt nix files

--- nix/profiles/rickbot.nix
+++ nix/profiles/rickbot.nix
@@ -6,9 +6,7 @@
 
   virtualisation.oci-containers.containers.rickbot = {
     image = "ghcr.io/rickvieira21/rickbot:latest";
-    environmentFiles = [
-      config.age.secrets.discordTokenEnv.path
-    ];
+    environmentFiles = [ config.age.secrets.discordTokenEnv.path ];
     login = {
       username = "RageKnify";
       registry = "ghcr.io";

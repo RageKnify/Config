77d681bf47004623b1783abe939eebc55574793f
RageKnify <RageKnify@gmail.com>
2023-07-06 01:29:51 +0100

    nix: cleanup home personal

--- nix/modules/home/personal.nix
+++ nix/modules/home/personal.nix
@@ -5,31 +5,29 @@
 #
 # personal home configuration, activates if system module is active
 
-{ pkgs, lib, osConfig, profiles, ... }:
+{ pkgs, lib, osConfig, ... }:
 let
   personal = osConfig.modules.personal;
-  inherit (lib) lists;
-in {
-  home.packages = with pkgs;
-    lists.optionals personal.enable [
-      # agenix
-      agenix
-      # calc
-      libqalculate
-      # wallpaper script
-      horseman_wallpaper
-      # LaTeX
-      texlive.combined.scheme-full
-      texlab
-      # Rust
-      rustup
-      # GCC
-      gcc
-      # Make
-      gnumake
-      # Python
-      my_python
-      # nixfmt
-      nixfmt
-    ];
+in lib.attrsets.optionalAttrs personal.enable {
+  home.packages = with pkgs; [
+    # agenix
+    agenix
+    # calc
+    libqalculate
+    # wallpaper script
+    horseman_wallpaper
+    # LaTeX
+    texlive.combined.scheme-full
+    texlab
+    # Rust
+    rustup
+    # GCC
+    gcc
+    # Make
+    gnumake
+    # Python
+    my_python
+    # nixfmt
+    nixfmt
+  ];
 }

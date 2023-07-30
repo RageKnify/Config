92aafc185a8abcb5eb26c64c6813f967a3475c70
RageKnify <RageKnify@gmail.com>
2023-07-30 13:14:55 +0100

    nix: remove lazarus' secrets

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,7 +1,5 @@
 let
   hosts = {
-    lazarus =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIII5eWrA8c9LJzVw/jV6CvMTDecRbcVBufdqUNIZVvBu";
     war =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIq8BgoZjQqFP0/QDJJ7TIT3r2k89uj2XcKBm4v3G2f0";
     azazel =
@@ -12,8 +10,6 @@ let
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2sdJFvvnEIYztPcznXvKpY4vOWedZ1qzDaAgRxrczS";
   };
 in {
-  "lazarus/restic_default_password.age".publicKeys =
-    [ hosts.lazarus users.jp_war ];
   "azazel/mailJoaoHashedPassword.age".publicKeys =
     [ hosts.azazel users.jp_war ];
   "azazel/mailWarHashedPassword.age".publicKeys = [ hosts.azazel users.jp_war ];

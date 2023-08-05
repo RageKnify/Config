78aac6fc43511c348145ff0825df64dbc9b5dd4e
RageKnify <RageKnify@gmail.com>
2023-08-05 21:41:19 +0100

    nix: fish: search for mountpoint boot

--- nix/profiles/home/fish.nix
+++ nix/profiles/home/fish.nix
@@ -22,7 +22,7 @@
         echo -e " \\e[1mDisk usage:\\e[0m"
         # echo
         echo -ne (\
-          df -l -h | grep -E 'dev/nvme0n1|/dev/vda1' | \
+          df -l -h | grep -E 'boot' | \
           awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
           sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
           paste -sd '''\

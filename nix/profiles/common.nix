c8b657bc629b1a3e498ed53ce80b9e4c1f39a594
RageKnify <RageKnify@gmail.com>
2023-10-17 15:53:07 +0100

    nix: update rnl cert file hash

--- nix/profiles/common.nix
+++ nix/profiles/common.nix
@@ -89,7 +89,7 @@
   security.pki.certificateFiles = [
     (builtins.fetchurl {
       url = "https://rnl.tecnico.ulisboa.pt/ca/cacert/cacert.pem";
-      sha256 = "020vnbvjly6kl0m6sj4aav05693prai10ny7hzr7n58xnbndw3j2";
+      sha256 = "1jiqx6s86hlmpp8k2172ki6b2ayhr1hyr5g2d5vzs41rnva8bl63";
     })
   ];
 

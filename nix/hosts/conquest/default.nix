d530702e84dd55a16c72cf3ce4976bbace274040
RageKnify <RageKnify@gmail.com>
2024-01-03 12:53:02 +0000

    nix: conquest: fmt

--- nix/hosts/conquest/default.nix
+++ nix/hosts/conquest/default.nix
@@ -45,7 +45,7 @@
     open = false;
 
     # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.

79e9c0b1776d5fa6d2a5f2789a0a12b90838d8f6
RageKnify <RageKnify@gmail.com>
2024-02-22 17:19:32 +0000

    nix: update firefly III

--- nix/overlays/firefly-data-importer/package.nix
+++ nix/overlays/firefly-data-importer/package.nix
@@ -1,5 +1,5 @@
 { pkgs, fetchFromGitHub, phpPackage ? pkgs.php83, ... }:
-let version = "1.4.0";
+let version = "1.4.4";
 in phpPackage.buildComposerProject (finalAttrs: {
   inherit version;
   pname = "firefly-iii-data-importer";
@@ -8,9 +8,9 @@ in phpPackage.buildComposerProject (finalAttrs: {
     owner = "firefly-iii";
     repo = "data-importer";
     rev = "v${version}";
-    hash = "sha256-tXHj0QsIywiGfm4nEPw6auM0Bw8NoCJdDTH7R59Krjc=";
+    hash = "sha256-hR17EL/4XdT5aewdKx+5ytSjWUGFKxh3Ht0qghWolTQ=";
   };
-  vendorHash = "sha256-dziaNdbWAYivH6WBUD5ePfJNK+3VWiNjrlgBoCliVeE=";
+  vendorHash = "sha256-oVLA1uhhDzPuNqaNtnjijFM0cx2jrRSbY0ptfjkhBIU=";
 
   # they have some version constraints that are frowned upon apparently
   composerStrictValidation = false;

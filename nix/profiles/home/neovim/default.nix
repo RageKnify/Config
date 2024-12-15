a4335e3abe5c0379c6d9b8993432f8808c33d705
RageKnify <RageKnify@gmail.com>
2024-12-15 00:41:54 +0000

    nix: nvim: update for 0.10 features
    - add substitute.nvim, helpful for manual find and replace
    - remove bindings that have equivalent bindings built-in
    - remove unused plugins

--- nix/profiles/home/neovim/default.nix
+++ nix/profiles/home/neovim/default.nix
@@ -5,9 +5,20 @@
 #
 # neovim home configuration.
 
-{ pkgs, lib, config, osConfig, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  osConfig,
+  ...
+}:
 let
-  inherit (lib) mkEnableOption mkIf lists strings;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    lists
+    strings
+    ;
   personal = osConfig.modules.personal.enable;
   git = config.modules.git.enable;
   commonPlugins = with pkgs.vimPlugins; [
@@ -15,19 +26,22 @@ let
     {
       plugin = lualine-nvim;
       type = "lua";
-      config = ''
-        vim.o.laststatus=2
-        vim.o.showtabline=2
-        vim.o.showmode=false
-        require'lualine'.setup {
-          options = {
-            theme = 'auto',
-            section_separators = {left='', right=''},
-            component_separators = {left='', right=''},
-            icons_enabled = true
-          },
-          sections = {
-            lualine_b = { '' + (strings.optionalString git "'diff'") + ''
+      config =
+        ''
+          vim.o.laststatus=2
+          vim.o.showtabline=2
+          vim.o.showmode=false
+          require'lualine'.setup {
+            options = {
+              theme = 'auto',
+              section_separators = {left='', right=''},
+              component_separators = {left='', right=''},
+              icons_enabled = true
+            },
+            sections = {
+              lualine_b = { ''
+        + (strings.optionalString git "'diff'")
+        + ''
           },
              lualine_c = {
                {'diagnostics', {
@@ -45,11 +59,14 @@ let
            },
            tabline = {
              lualine_a = { 'hostname' },
-             lualine_b = { '' + (strings.optionalString git "'branch'") + ''
-            },
-               lualine_z = { {'tabs', tabs_color = { inactive = "TermCursor", active = "ColorColumn" } } }
-             },
-             extensions = { fzf'' + (strings.optionalString git ", fugitive ")
+             lualine_b = { ''
+        + (strings.optionalString git "'branch'")
+        + ''
+          },
+             lualine_z = { {'tabs', tabs_color = { inactive = "TermCursor", active = "ColorColumn" } } }
+           },
+           extensions = { fzf''
+        + (strings.optionalString git ", fugitive ")
         + ''
           },
           }
@@ -89,6 +106,7 @@ let
       nvim-treesitter = pkgs.vimPlugins.nvim-treesitter;
     })
 
+    # place marks at the left
     vim-signature
 
     {
@@ -167,41 +185,23 @@ let
       '';
     }
 
+    {
+      plugin = substitute-nvim;
+      type = "lua";
+      config = ''
+      require('substitute').setup{}
+      vim.keymap.set("n", "s", require('substitute').operator, { noremap = true })
+      vim.keymap.set("n", "ss", require('substitute').line, { noremap = true })
+      vim.keymap.set("n", "S", require('substitute').eol, { noremap = true })
+      vim.keymap.set("x", "s", require('substitute').visual, { noremap = true })
+      '';
+    }
+
+    # lib used by other plugins
     plenary-nvim
-
-    {
-      plugin = nvim-osc52;
-      type = "lua";
-      config = ''
-        local function copy(lines, _)
-          require('osc52').copy(table.concat(lines, '\n'))
-        end
-
-        local function paste()
-          return {vim.fn.split(vim.fn.getreg(""), '\n'), vim.fn.getregtype("")}
-        end
-
-        vim.g.clipboard = {
-          name = 'osc52',
-          copy = {['+'] = copy, ['*'] = copy},
-          paste = {['+'] = paste, ['*'] = paste},
-        }
-
-        -- Now the '+' register will copy to system clipboard using OSC52
-        vim.keymap.set('n', '<leader>y', '"+y')
-        vim.keymap.set('x', '<leader>y', '"+y')
-      '';
-    }
-
-    {
-      plugin = nvim-colorizer-lua;
-      type = "lua";
-      config = ''
-        require 'colorizer'.setup ({ user_default_options = { names = false; }})
-      '';
-    }
   ];
-  personalPlugins = with pkgs.vimPlugins;
+  personalPlugins =
+    with pkgs.vimPlugins;
     lists.optionals personal [
       {
         plugin = nvim-lspconfig;
@@ -215,23 +215,13 @@ let
           lsp_config.ocamllsp.setup(lsp_setup)
 
           -- Rust lsp setup
-          local rt = require("rust-tools")
-
-          local capabilities = lsp_setup.capabilities
-          local on_attach = lsp_setup.on_attach
-          rt.setup({
-            server = {
-              capabilities = capabilities,
-              on_attach = function(_, bufnr)
-                -- Hover actions
-                on_attach(_, bufnr)
-                vim.keymap.set('n', 'K', rt.hover_actions.hover_actions, {silent=true})
-              end,
-              settings = {
-                ["rust-analyzer"] = {
-                  checkOnSave = {
-                    command = "clippy",
-                  },
+          lsp_config.rust_analyzer.setup({
+            capabilities = lsp_setup.capabilities,
+            on_attach = lsp_setup.on_attach,
+            settings = {
+              ["rust-analyzer"] = {
+                check = {
+                  command = "clippy",
                 },
               },
             },
@@ -239,18 +229,6 @@ let
 
           -- tex lsp setup
           lsp_config.texlab.setup(lsp_setup)
-        '';
-      }
-      rust-tools-nvim
-
-      {
-        plugin = presence-nvim;
-        config = ''
-          let g:presence_auto_update       = 1
-          let g:presence_editing_text      = "Editing %s"
-          let g:presence_workspace_text    = "Working on %s"
-          let g:presence_neovim_image_text = "vim but better"
-          let g:presence_main_image        = "neovim"
         '';
       }
 
@@ -321,19 +299,39 @@ let
     setlocal tabstop=2
     setlocal expandtab
   '';
-  files = {
-    "${config.xdg.configHome}/nvim/lua/generic_lsp.lua".source =
-      ./generic_lsp.lua;
-    "${config.xdg.configHome}/nvim/after/ftplugin/nix.vim".text = ''
-      nnoremap <silent> <leader>tt :silent !${pkgs.nixfmt-rfc-style}/bin/nixfmt %<CR>
-    '' + twoSpaceIndentConfig;
-  } //
+  files =
+    {
+      "${config.xdg.configHome}/nvim/lua/generic_lsp.lua".source = ./generic_lsp.lua;
+      "${config.xdg.configHome}/nvim/after/ftplugin/nix.vim".text =
+        ''
+          nnoremap <silent> <leader>tt :silent !${pkgs.nixfmt-rfc-style}/bin/nixfmt %<CR>
+        ''
+        + twoSpaceIndentConfig;
+    }
+    //
     # languages that should use 2 space indent
-    builtins.listToAttrs (builtins.map (filetype: {
-      name = "${config.xdg.configHome}/nvim/after/ftplugin/${filetype}.vim";
-      value = { text = twoSpaceIndentConfig; };
-    }) [ "markdown" "ocaml" "wast" "yaml" "yacc" "lex" "cpp" "tex" "scheme" ]);
-in {
+    builtins.listToAttrs (
+      builtins.map
+        (filetype: {
+          name = "${config.xdg.configHome}/nvim/after/ftplugin/${filetype}.vim";
+          value = {
+            text = twoSpaceIndentConfig;
+          };
+        })
+        [
+          "markdown"
+          "ocaml"
+          "wast"
+          "yaml"
+          "yacc"
+          "lex"
+          "cpp"
+          "tex"
+          "scheme"
+        ]
+    );
+in
+{
   programs.neovim = {
     package = pkgs.neovim-unwrapped;
     enable = true;
@@ -341,32 +339,41 @@ in {
     vimAlias = true;
     vimdiffAlias = true;
     extraConfig = builtins.readFile ./base.vim;
-    plugins = commonPlugins ++ personalPlugins ++ (with pkgs.vimPlugins;
-      lists.optionals git [
-        vim-fugitive
-        {
-          plugin = gitsigns-nvim;
-          type = "lua";
-          config = ''
-            require('gitsigns').setup{
-              signs = {
-                add = {  text = '+' },
-              },
-              on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
+    extraLuaConfig = ''
+      vim.keymap.set('n', '<leader>y', '"+y')
+      vim.keymap.set('x', '<leader>y', '"+y')
+    '';
+    plugins =
+      commonPlugins
+      ++ personalPlugins
+      ++ (
+        with pkgs.vimPlugins;
+        lists.optionals git [
+          vim-fugitive
+          {
+            plugin = gitsigns-nvim;
+            type = "lua";
+            config = ''
+              require('gitsigns').setup{
+                signs = {
+                  add = {  text = '+' },
+                },
+                on_attach = function(bufnr)
+                  local gs = package.loaded.gitsigns
 
-                local function map(mode, l, r, opts)
-                  opts = opts or {}
-                  opts.buffer = bufnr
-                  vim.keymap.set(mode, l, r, opts)
-                end
+                  local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                  end
 
-                map('n', '<leader>gb', gs.toggle_current_line_blame)
-              end,
-            }
-          '';
-        }
-      ]);
+                  map('n', '<leader>gb', gs.toggle_current_line_blame)
+                end,
+              }
+            '';
+          }
+        ]
+      );
   };
 
   home.file = files;
